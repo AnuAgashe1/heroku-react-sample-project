a3ad473b67d0ff6a8803bfe38896c2e67bab15eb
AnuAgashe1 <anuagashe1@gmail.com>
2018-03-13 18:11:07 +0530

    test auto deploy

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+//import MongoJs from 'mongojs';
+//import router from 'express.Router()';
+
+//var express = require('express');
+//var router = express.Router();
+//var mongojs = require('mongojs');
+//var db = mongojs('mongodb://localhost:27017/admin', ['User']);
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       message: null,
-      fetching: true
+      fetching: true,
     };
+    
   }
 
   componentDidMount() {
@@ -31,7 +39,27 @@ class App extends Component {
         });
       })
   }
-
+  addRecord(){
+    debugger;
+    window.alert('OnClick function');
+  //   router.post('/new', function(req, res, next){
+  //     debugger;
+  //     var todo = req.body;
+  //     if(!todo.title || !(todo.isDone + '')){
+  //         res.status(400);
+  //         res.json({
+  //             "error": "Bad Data"
+  //         });
+  //     } else {
+  //         db.todos.save(todo, function(err, todo){
+  //             if(err){
+  //                 res.send(err);
+  //             }
+  //             res.json(todo);
+  //         });
+  //     }
+  // });
+  }
   render() {
     return (
       <div className="App">
@@ -40,7 +68,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          {'This is first try'}
+          {'This is test deploy'}
           <a href="https://github.com/mars/heroku-cra-node">
             {'create-react-app with a custom Node/Express server'}
           </a><br/>
@@ -50,6 +78,7 @@ class App extends Component {
             ? 'Fetching message from API'
             : this.state.message}
         </p>
+        <button onClick={this.addRecord}>Add Record</button>
       </div>
     );
   }
